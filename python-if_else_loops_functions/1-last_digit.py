8552031105c591eb54bbc5a40c96e49342609f83
kriaa9 <11596@holbertonstudents.com>
2026-01-17 21:50:42 +0100

    Remove unnecessary comment in last digit calculation for negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 last_digit = number % 10
 
 if number < 0:
-    last_digit = -(-number % 10)  # Get the last digit and keep it negative
+    last_digit = -(-number % 10)
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
