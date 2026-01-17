69b5249556a1867c00175f91d5790291c3f0362d
kriaa9 <11596@holbertonstudents.com>
2026-01-17 21:49:53 +0100

    Remove unnecessary comment about adjusting last_digit for negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,6 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 
-# Adjust last_digit for negative numbers
 if number < 0:
     last_digit = -(-number % 10)  # Get the last digit and keep it negative
 
