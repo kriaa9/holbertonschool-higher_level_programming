761ce3d1df3d92bae280490f6feb754f8e774bdb
kriaa9 <11596@holbertonstudents.com>
2026-01-18 20:39:20 +0100

    Add pow function to compute the power of a number

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Compute a to the power of b and return the value.
+
+    Args:
+        a: Base number
+        b: Exponent
+
+    Returns:
+        The value of a raised to the power of b
+    """
+    return a ** b
