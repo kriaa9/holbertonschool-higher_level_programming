541be4afd1fb7aae308cfaf7c996e4f6df020c18
kriaa9 <11596@holbertonstudents.com>
2026-01-18 20:40:41 +0100

    Add fizzbuzz function to print numbers from 1 to 100 with Fizz, Buzz, FizzBuzz rules

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers from 1 to 100 with Fizz, Buzz, FizzBuzz rules.
+
+    For multiples of 3: print Fizz
+    For multiples of 5: print Buzz
+    For multiples of both 3 and 5: print FizzBuzz
+    Otherwise: print the number
+    Each element is followed by a space.
+    """
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
