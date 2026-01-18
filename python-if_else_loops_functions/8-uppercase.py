add2db881b9bdcc80104c4ac71b48c0412b8282f
kriaa9 <11596@holbertonstudents.com>
2026-01-18 20:32:47 +0100

    Refactor uppercase function for improved readability

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -8,12 +8,7 @@ def uppercase(str):
     Prints:
         The string in uppercase
     """
-    for i in range(len(str)):
-        c = str[i]
-        if ord(c) >= 97 and ord(c) <= 122:
-            # Lowercase letter: convert to uppercase
-            print("{}".format(chr(ord(c) - 32)), end='')
-        else:
-            # Not lowercase: print as is
-            print("{}".format(c), end='')
+    for c in str:
+        print("{}".format(chr(ord(c) - 32) if ord(c) >= 97 and
+                          ord(c) <= 122 else c), end='')
     print()
