4eae8ea021c0a549a80cfab9452fb19e9a8de51b
kriaa9 <11596@holbertonstudents.com>
2026-01-23 19:06:54 +0000

    Fix pycodestyle whitespace error

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,12 +2,11 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
-    # Assume the first one is the biggest to start
+
     big = my_list[0]
-    
+
     for i in my_list:
         if i > big:
             big = i
-            
+
     return big
