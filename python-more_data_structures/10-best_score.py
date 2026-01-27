c026161c5fed3c7c51c1e914ecdb2b247d2fc5de
kriaa9 <11596@holbertonstudents.com>
2026-01-27 16:49:36 +0100

    Fix pycodestyle whitespace error

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Return None if the dictionary is empty or None
     if not a_dictionary:
         return None
-    
-    # Initialize variables to store the best key and value found so far
-    # We can use the first key in the dictionary as a starting point
+
     best_key = list(a_dictionary.keys())[0]
     best_val = a_dictionary[best_key]
-    
+
     for key, value in a_dictionary.items():
         if value > best_val:
             best_val = value
             best_key = key
-            
+
     return best_key
