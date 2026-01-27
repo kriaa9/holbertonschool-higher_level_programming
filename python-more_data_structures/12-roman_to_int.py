7a38b1b6c167ee83bb4cb89f56bbe42b2a08be28
kriaa9 <11596@holbertonstudents.com>
2026-01-27 17:46:03 +0100

    Fix pycodestyle whitespace error in Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,19 +7,19 @@ def roman_to_int(roman_string):
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
+
     result = 0
     prev_value = 0
-    
+
     # Iterate from the last character to the first
     for char in reversed(roman_string):
         value = roman_dict.get(char, 0)
-        
+
         if value >= prev_value:
             result += value
         else:
             result -= value
-            
+
         prev_value = value
-        
+
     return result
