53912fbe8f299fe645de533cd96ecf46244dc571
kriaa9 <11596@holbertonstudents.com>
2026-01-27 20:22:19 +0100

    Fix trailing whitespace in print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -18,7 +18,7 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
