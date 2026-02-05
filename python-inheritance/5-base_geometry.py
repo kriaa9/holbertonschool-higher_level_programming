204f1dca5ed20b04804c2bf2cf4f792147f36c6a
kriaa9 <11596@holbertonstudents.com>
2026-02-05 20:12:19 +0100

    Add BaseGeometry class and main script for testing

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines a base geometry class.
+
+This module contains the BaseGeometry class which serves as a base
+class for geometry-related classes. It is currently empty and will
+be extended in future tasks.
+"""
+
+
+class BaseGeometry:
+    """An empty base geometry class.
+
+    This class serves as a foundation for geometry-related classes
+    and will be extended with methods and attributes in future tasks.
+    """
+
+    pass
