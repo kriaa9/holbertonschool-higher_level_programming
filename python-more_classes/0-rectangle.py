714d5e9eb61d54c1b9ac2a5ca2c6f4872b99434b
kriaa9 <11596@holbertonstudents.com>
2026-02-05 18:16:41 +0100

    Remove unnecessary blank lines in Rectangle class module

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Module that defines an empty Rectangle class.
-
 This module contains a simple Rectangle class with no attributes or methods.
 It serves as a starting point for building more complex rectangle implementations.
 """
 
 class Rectangle:
     """An empty class that defines a rectangle.
-
     This class currently has no attributes or methods.
     It will be extended in subsequent tasks.
     """
