71902e9377f92bcbed9271070ba8e48dab60cd2d
kriaa9 <11596@holbertonstudents.com>
2026-02-11 11:40:19 +0100

    Add task 10: Square class implementation

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a Square class that inherits from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a Square with validated size.
+
+        Args:
+            size: The size of the square (both width and height).
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+
