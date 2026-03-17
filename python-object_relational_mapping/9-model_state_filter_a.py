adc9877b50209d77ab5bd74dfe066459d78038bf
kriaa9 <11596@holbertonstudents.com>
2026-03-17 13:19:13 +0100

    Fix: Reformat query for better readability in state filter script

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,9 +3,9 @@
 Lists all State objects that contain the letter a from the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     session = Session()
 
     # Filter for states containing 'a' and order by id
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%')) \
+        .order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
