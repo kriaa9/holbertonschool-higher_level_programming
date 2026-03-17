b30aeb34ebac351285989700f72029ae528f81d0
kriaa9 <11596@holbertonstudents.com>
2026-03-17 13:27:11 +0100

    Refactor: Improve docstring formatting and streamline state query logic

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
 """
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
@@ -14,10 +15,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')) \
-        .order_by(State.id).all()
-
-    for state in states:
+    for state in session.query(State).order_by(State.id).filter(
+            State.name.like('%a%')):
         print("{}: {}".format(state.id, state.name))
 
     session.close()
