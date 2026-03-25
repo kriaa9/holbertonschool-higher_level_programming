009dc3e5bf4fa0af491315db55f6fd2a45cf3482
kriaa9 <11596@holbertonstudents.com>
2026-03-25 10:42:02 +0100

    Fix: Update indentation for consistency in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argCount = process.argv.length - 2; // substract 2 to ignore 'node' and script name
+const argCount = process.argv.length - 2;
 
 if (argCount === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argCount === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
